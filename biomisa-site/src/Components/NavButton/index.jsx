036863c650795d84eed14eb7aab459707ce1a95c
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-03-18 15:20:49 -0300

    selected

--- biomisa-site/src/Components/NavButton/index.jsx
+++ biomisa-site/src/Components/NavButton/index.jsx
@@ -6,12 +6,13 @@ import { NavLink } from 'react-router-dom'
 export default (props) => {
 
 
+
   return (
 
     <div className='nav-button'>
       <p><NavLink to={props.link} > {props.children} </NavLink></p>
 
-        <div className="border--bar">
+        <div className="nav-button--border-bar">
 
         </div>
 
