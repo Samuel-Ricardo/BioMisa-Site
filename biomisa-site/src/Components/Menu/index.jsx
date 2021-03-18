d8b2bae322696e86ccc331fc0a9a7d9fdc32c07d
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-03-18 12:31:37 -0300

    Add: Images - Credits

--- biomisa-site/src/Components/Menu/index.jsx
+++ biomisa-site/src/Components/Menu/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Menu.css'
 
-export default () => {
+export default (props) => {
 
   return (
 
--- biomisa-site/src/Images/index.jsx
+++ biomisa-site/src/Images/index.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 import BiomisaBanner from './Biomisa-banner.jpeg'
 import Biomisa from './Biomisa.jpeg'
+import Samuel from './Samuel.png'
+import Milena from './Milena.png'
+import Deborah from './Debbye.png'
 
 export default {
 
   BIOMISA_BANNER: BiomisaBanner,
+  BIOMISA_LOGO: Biomisa,
 
-  BIOMISA_LOGO: Biomisa
-
+  SAMUEL: Samuel,
+  MILENA: Milena,
+  DEBORA: Deborah
 }
