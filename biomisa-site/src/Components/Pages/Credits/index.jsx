ac1d54587784d79e4c599aa014da6c425b4b5b98
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-03-25 14:35:02 -0300

    background gradient

--- biomisa-site/src/Components/Pages/Credits/index.jsx
+++ biomisa-site/src/Components/Pages/Credits/index.jsx
@@ -19,10 +19,32 @@ const CreditsPage = (props) => {
 
       <section className='Credits--Section' style={{ paddingLeft: 30 }}>
 
-        <h1 className='Credits-Title'> Criadores </h1>
+          <section id="Authors">
 
-          <section id="Samuel--Programmer">
-            <img src={Images.SAMUEL} alt="Samuel"/>
+            <h1 className='Credits--Authors-Title'> Equipe de Desenvolvedores </h1>
+
+            <section id="Programmers">
+
+              <h2 className='Credits--Programmers-Title'>Programadores</h2>
+
+              <section id="Samuel--Programmer">
+                <img src={Images.SAMUEL} alt="Samuel" />
+
+                <p>
+                  Samuel Ricardo
+                </p>
+
+                <p>
+
+                  A roda de cores (ou imagem na guia Extrair tema) pode ser usada para gerar paleta de cores, a qual pode ser salva na Creative Cloud após o logon.
+                  Depois, você pode usar seus temas de cores salvos nos produtos da Adobe, como o Photoshop, o Illustrator, o Fresco etc., por meio do painel de temas do Adobe Color ou das Bibliotecas da CC.
+                </p>
+
+                <p>A roda de cores (ou imagem na guia Extrair tema) pode ser usada para gerar paleta de cores, a qual pode ser salva na Creative Cloud após o logon.
+</p>
+              </section>
+
+            </section>
           </section>
 
       </section>
