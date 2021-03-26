60f35fb125e9992747eae41cbcef24cc1754eb88
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-03-26 09:44:16 -0300

    SVG

---
+++ biomisa-site/src/Components/SVG/Facebook.jsx
@@ -0,0 +1,31 @@
+import React from 'rect'
+
+export default () => {
+
+  return (
+
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      xmlns:svgjs="http://svgjs.com/svgjs"
+      version="1.1"
+      width="512"
+      height="512"
+      x="0" y="0"
+      viewBox="0 0 512 512"
+      style="enable-background:new 0 0 512 512"
+      xml:space="preserve"
+      class="">
+
+        <g>
+        <path
+          xmlns="http://www.w3.org/2000/svg"
+          d="m437 0h-362c-41.351562 0-75 33.648438-75 75v362c0 41.351562 33.648438 75 75 75h151v-181h-60v-90h60v-61c0-49.628906 40.371094-90 90-90h91v90h-91v61h91l-15 90h-76v181h121c41.351562 0 75-33.648438 75-75v-362c0-41.351562-33.648438-75-75-75zm0 0"
+          fill="#000000"
+          data-original="#000000"
+          style="" class="" />
+        </g>
+
+    </svg>
+  )
+ }
