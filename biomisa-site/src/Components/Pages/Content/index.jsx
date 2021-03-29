5ba5a7955bb1c169f71bafe46013a68b68958aa9
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-03-29 12:42:25 -0300

    Text Decoration

--- biomisa-site/src/Components/Pages/Content/index.jsx
+++ biomisa-site/src/Components/Pages/Content/index.jsx
@@ -58,7 +58,7 @@ const ContentPage = (props) => {
 
             <h1 id='Introduction'> Sistema Nervoso </h1>
 
-              <p>
+              <p id='intro'>
 
                 O sistema nervoso é o sistema do corpo humano encarregado de captar e gerar respostas diante de estímulos que encontramos externamente e internamente, com esse sistema somos capazes de reagir e sentir diferentes sensações e alterações em nosso ambiente. O mesmo é composto por um tipo de tecido, denominado “tecido nervoso”, o qual possui como tipo celulares, os neurônios e as chamadas células da glia. Os neurônios são responsáveis pela propagação de impulsos nervosos e têm como partes básicas o corpo celular, onde se encontra: O núcleo; Os axônios e os dendritos.
 
@@ -68,7 +68,7 @@ const ContentPage = (props) => {
 
                 <h3 id='system--division'> O sistema nervoso é dividido em duas partes </h3>
 
-                <ul>
+                <ul className='list'>
                   <li> <a href="#SNC"><Bold> Sistema Nervoso Central: </Bold></a> formado pelo encéfalo e medula espinhal</li>
 
                   <li> <a href="#SNP"><Bold> Sistema Nervoso Periférico: </Bold></a> formado pelos nervos, gânglios e terminações nervosas.</li>
